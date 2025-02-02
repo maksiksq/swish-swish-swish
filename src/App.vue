c734c9952ed818bd26760365c0e804da977d1a94
Maksiks <maksiksking@gmail.com>
2025-02-02 19:21:42 +0200

    working for images now

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,103 @@
-<script setup>
-import {onMounted, ref} from "vue";
+<script lang="ts" setup>
 import {
   GestureRecognizer,
   FilesetResolver,
   DrawingUtils
-} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
+} from "@mediapipe/tasks-vision"
+import {
+  warn,
+  debug,
+  trace,
+  info,
+  error,
+  attachConsole,
+  attachLogger,
+} from '@tauri-apps/plugin-log';
+import {onMounted, ref} from "vue";
 
-let gestureRecognizer = GestureRecognizer;
-let runningMode = "VIDEO";
-let isWebcamRunning = true;
-const videoHeight = "360px";
-const videoWidth = "480px";
+const pTxt = ref(null);
 
-let canvasCtx;
+let gestureRecognizer: GestureRecognizer;
+let runningMode = "IMAGE";
 
-const video = ref(null)
-
-const canvasElement = ref(null)
-const canvasCtxRef = ref(null);
-
-const gestureOutput = ref(null)
-
+// one goal would be to make
+// 1) Model from scratch (help)
+// 2) Replace the vision locally (easy)
 const createGestureRecognizer = async () => {
-  const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
-  );
+
+  const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm") // maybe replace locally?
+
   gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
     baseOptions: {
-      modelAssetPath:
-          "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+      modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
       delegate: "GPU"
     },
     runningMode: runningMode
   });
-}
+  info('gestureRecognizer initialized');
+};
 
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-}
-
-const stream = ref(null);
-
-async function startCamera() {
-  try {
-    stream.value = await navigator.mediaDevices.getUserMedia({ video: true });
-
-    if (video.value) {
-      video.value.srcObject = stream.value;
-      isWebcamRunning = true;
-    }
-  } catch (error) {
-    console.error("Error accessing the camera:", error);
-  }
-}
-
-function enableCam(event) {
+async function handleClick(e) {
   if (!gestureRecognizer) {
-    alert("Почекай, грузиться.")
+    warn("It's loading, hold on a little");
     return;
   }
 
-
-}
-
-let lastVideoTime = -1;
-let results = undefined;
-async function predictWebcam() {
-  const webcamElement = video;
-  // Now let's start detecting the stream.
-  if (runningMode === "IMAGE") {
-    runningMode = "VIDEO";
-    await gestureRecognizer.setOptions({ runningMode: "VIDEO" });
-  }
-  let nowInMs = Date.now();
-  if (video.currentTime !== lastVideoTime) {
-    lastVideoTime = video.currentTime;
-    results = gestureRecognizer.recognizeForVideo(video, nowInMs);
+  if (runningMode === "VIDEO") {
+    runningMode = "IMAGE";
+    await gestureRecognizer.setOptions({ runningMode: "IMAGE" });
   }
 
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  const drawingUtils = new DrawingUtils(canvasCtx);
+  const allCanvas = e.target.parentNode.getElementsByClassName("canvas");
+  for (var i = allCanvas.length - 1; i >= 0; i--) {
+    const n = allCanvas[i];
+    n.parentNode.removeChild(n);
+  }
 
-  canvasElement.style.height = videoHeight;
-  webcamElement.style.height = videoHeight;
-  canvasElement.style.width = videoWidth;
-  webcamElement.style.width = videoWidth;
+  const results = gestureRecognizer.recognize(e.target);
+  // info(`gestureRecognizer ${JSON.stringify(results)}`);
 
-  if (results.landmarks) {
+  if (results.gestures.length > 0) {
+    const p = pTxt.value // reassigned this to a vue ref cause Typescript and Vue
+
+    info(p)
+
+    p.setAttribute("class", "info");
+
+    const categoryName = results.gestures[0][0].categoryName;
+    // has to be stringified, idk why haven't they done it in the example codepen
+    const categoryScore = parseFloat((results.gestures[0][0].score * 100).toString()).toFixed(2);
+    const handedness = results.handednesses[0][0].displayName;
+
+    p.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore}%\n Handedness: ${handedness}`;
+    p.style =
+        "left: 0px;" +
+        "top: " +
+        e.target.height +
+        "px; " +
+        "width: " +
+        (e.target.width - 10) +
+        "px;";
+
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("class", "canvas");
+    canvas.setAttribute("width", e.target.naturalWidth + "px");
+    canvas.setAttribute("height", e.target.naturalHeight + "px");
+
+    // added .cssText cause Typescript doesn't have .style on elems for some reason?
+    canvas.style.cssText =
+        "left: 0px;" +
+        "top: 0px;" +
+        "width: " +
+        e.target.width +
+        "px;" +
+        "height: " +
+        e.target.height +
+        "px;";
+
+    e.target.parentNode.appendChild(canvas);
+    const canvasCtx = canvas.getContext("2d");
+    const drawingUtils = new DrawingUtils(canvasCtx);
     for (const landmarks of results.landmarks) {
       drawingUtils.drawConnectors(
           landmarks,
@@ -99,189 +109,156 @@ async function predictWebcam() {
       );
       drawingUtils.drawLandmarks(landmarks, {
         color: "#FF0000",
-        lineWidth: 2
+        lineWidth: 1
       });
     }
   }
-  canvasCtx.restore();
-  if (results.gestures.length > 0) {
-    gestureOutput.style.display = "block";
-    gestureOutput.style.width = videoWidth;
-    const categoryName = results.gestures[0][0].categoryName;
-    const categoryScore = parseFloat(
-        results.gestures[0][0].score * 100
-    ).toFixed(2);
-    const handedness = results.handednesses[0][0].displayName;
-    gestureOutput.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore} %\n Handedness: ${handedness}`;
-  } else {
-    gestureOutput.style.display = "none";
-  }
-  // Call this function again to keep predicting when the browser is ready.
-  if (isWebcamRunning === true) {
-    window.requestAnimationFrame(predictWebcam);
-  }
 }
 
+
+
 onMounted(() => {
-  if (hasGetUserMedia()) {
-    console.log("cool")
-  } else {
-    console.warn("getUserMedia() is not supported by your browser");
-  }
-
-  canvasCtxRef.value = canvasElement.getContext("2d");
-  canvasCtx = canvasElement.getContext("2d")
-
-
-
+  info('alivency')
   createGestureRecognizer();
-  enableCam();
-  startCamera();
-
-  predictWebcam();
-
 })
 
-
+function sayHello() {
+  info('hello0')
+}
 </script>
 
 <template>
-  <header>
-    <h1><span>></span>Swish</h1>
-  </header>
   <main class="global-cont">
-    <div class="circle"></div>
-
-    <div class="cont">
-      <div>
-        <video id="webcam" ref="video" autoplay playsinline></video>
-        <canvas ref="canvasElement" class="output_canvas" id="output_canvas" width="1280" height="720" style="z-index: 10999; pointer-events: none; position: absolute; left: 0px; top: 0px;"></canvas>
-        <p ref="gestureOutput" id='gesture_output' class="output"></p>
-      </div>
-
-      <button @click="enableCam">yes</button>
+    <div class="detectOnClick">
+      <img @click="handleClick" src="https://assets.codepen.io/9177687/idea-gcbe74dc69_1920.jpg" crossorigin="anonymous" title="Click to get recognize!"  alt="ide stop bugging me"/>
+      <p ref="pTxt" class="classification removed"></p>
     </div>
+    <div class="detectOnClick">
+      <img @click="handleClick" src="https://assets.codepen.io/9177687/thumbs-up-ga409ddbd6_1.png" crossorigin="anonymous" title="Click to get recognize!" alt="ide stop bugging me" />
+      <p ref="pTxt" class="classification removed"></p>
+    </div>
+
+    <div id='liveView' class="videoView"></div>
+    <button @click="sayHello">Run</button>
   </main>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap');
-
-* {
-  margin: 0;
-  padding: 0;
-}
-
-html {
-  overflow-x: hidden;
-  overflow-y: hidden;
-}
-
 body {
-  background-color: #1d1d21;
-  overflow-x: hidden;
-  overflow-y: hidden;
-}
-
-h1 {
-  font-family: 'Roboto Mono', sans-serif;
-  font-weight: 800;
-
-  font-size: 1.2rem;
+  background-color: rgba(29, 29, 33, 1);
 }
 </style>
 <style lang="scss" scoped>
-h1 {
-  padding-left: 2vw;
-
-  span {
-    color: #c6c6c6;
-  }
-}
-
-:root {
-  color: white;
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-.circle { // it's an ellipse but yes
-  border-radius: 50%;
-  width: 40%;
-  height: 40vw;
-
-  position: absolute;
-  top: -7vh;
-  left: 2vw;
-  z-index: -1;
-
-  background-color: #390036;
-}
-
-header {
-  width: 100%;
-  height: 6vh;
-
-  color: white;
-
-  //background-image: linear-gradient(35deg in hsl, #390036 5%, #2d0f36 10%, #241732 15%, #1e1b2b 20%, #1d1d21);
-  background-color: rgba(29, 29, 33, 0.6);
-  backdrop-filter: blur(300px);
-  border-bottom: black 1px solid;
-
-  position: relative;
-  z-index: 10099;
-
+main {
   display: flex;
-  flex-direction: row;
-  //justify-content: flex-end;
-  align-items: center;
-}
-
-.cont {
-  width: 100%;
-  height: 94vh;
-
-  position: relative;
-  z-index: 10098;
-
-  //background-color: #1d1d21;
-
-  background-color: rgba(29, 29, 33, 0.6);
-  backdrop-filter: blur(1000px) grayscale(0.8);
-
-  display: flex;
+  justify-content: center;
   flex-direction: column;
   align-items: center;
 
-  button {
-    all: unset;
+  img {
+    width: 32vw;
+    height: 18vw; // that's how you do math (16:9)
 
-    width: 30vh;
-    height: 7vh;
     cursor: pointer;
-    border-radius: 4px;
-    border: 1px solid rgba(250, 250, 250, 0.3);
-
-    /* From https://css.glass */
-    background: rgb(44, 15, 74);
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(17.3px);
-    -webkit-backdrop-filter: blur(17.3px);
-
   }
+}
 
-  video {
-    width: 30vw;
-    height: 30vw;
-  }
+// copy pastad
+body {
+  font-family: roboto,serif;
+  margin: 2em;
+  color: #3d3d3d;
+  --mdc-theme-primary: #007f8b;
+  --mdc-theme-on-primary: #f1f3f4;
+}
+
+h1 {
+  color: #007f8b;
+}
+
+h2 {
+  clear: both;
+}
+
+video {
+  clear: both;
+  display: block;
+  transform: rotateY(180deg);
+  -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+  height: 280px;
+}
+
+section {
+  opacity: 1;
+  transition: opacity 500ms ease-in-out;
+}
+
+.removed {
+  display: none;
+}
+
+.invisible {
+  opacity: 0.2;
+}
+
+.detectOnClick {
+  position: relative;
+  float: left;
+  width: 48%;
+  margin: 2% 1%;
+  cursor: pointer;
+}
+.videoView {
+  position: absolute;
+  float: left;
+  width: 48%;
+  margin: 2% 1%;
+  cursor: pointer;
+  min-height: 500px;
+}
+
+.videoView p,
+.detectOnClick p {
+  padding-top: 5px;
+  padding-bottom: 5px;
+  background-color: #007f8b;
+  color: #fff;
+  border: 1px dashed rgba(255, 255, 255, 0.7);
+  z-index: 2;
+  margin: 0;
+}
+
+.highlighter {
+  background: rgba(0, 255, 0, 0.25);
+  border: 1px dashed #fff;
+  z-index: 1;
+  position: absolute;
+}
+
+.canvas {
+  z-index: 1;
+  position: absolute;
+  pointer-events: none;
 }
 
 .output_canvas {
   transform: rotateY(180deg);
   -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+}
+
+.detectOnClick {
+  z-index: 0;
+  font-size: calc(8px + 1.2vw);
+}
+
+.detectOnClick img {
+  width: 45vw;
+}
+.output {
+  display: none;
+  width: 100%;
+  font-size: calc(8px + 1.2vw);
 }
 </style>
