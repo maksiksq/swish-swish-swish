93745ed2e5a9ca5734b0be8994051d2facbce3f3
Maksiks <maksiksking@gmail.com>
2025-02-03 23:27:44 +0200

    supressed an error that isn't real

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,8 @@ const createGestureRecognizer = async () => {
       modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
       delegate: "GPU"
     },
+    // typescript being funni here, RunningMode type is basically a string.
+    // @ts-ignore
     runningMode: runningMode
   });
   info('gestureRecognizer initialized');
